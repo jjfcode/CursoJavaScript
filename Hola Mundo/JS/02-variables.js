a4ee5ffe25e8f44e32335329f2360826ed157a5b
John Ferlito <jjfcode@gmail.com>
2023-07-25 21:46:32 -0400

    18 variables y comentarios

---
+++ Hola Mundo/JS/02-variables.js
@@ -0,0 +1,12 @@
+// VARIABLES
+// Una variable es un contenedor de informacion
+
+var pais = "Espana";
+var continente = "Europa";
+var antiguedad = 2019;
+var pais_y_continente = pais+' '+continente;
+
+pais = "USA";
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);

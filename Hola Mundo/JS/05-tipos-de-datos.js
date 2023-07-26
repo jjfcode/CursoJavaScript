5b989f0e606473c6338941ef6afc6c529e9e2548
John Ferlito <jjfcode@gmail.com>
2023-07-25 22:23:57 -0400

    22 Operadores y tipos de datos

---
+++ Hola Mundo/JS/05-tipos-de-datos.js
@@ -0,0 +1,16 @@
+'use strict'
+
+// Operadores
+var numero1 = 7;
+var numero2 = 12;
+var operacion = numero1 * numero2
+
+alert('El resultado de la operacion es: '+operacion);
+
+// Tipos de Datos
+var numero_entero = 44;
+var cadena_texto = "hola que tal";
+var verdadero_o_falso = true; // o false
+
+var numero_falso = "33";
+console.log(Number(numero_falso)+7);

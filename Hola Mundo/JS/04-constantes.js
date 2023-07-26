8c7e154bfa068ced79f46fef04264a92a3ca958c
John Ferlito <jjfcode@gmail.com>
2023-07-25 22:06:00 -0400

    21 constantes

---
+++ Hola Mundo/JS/04-constantes.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// Constantes
+// Como una variable, pero su valor no puede cambiar
+
+var web = "https://victorroblesweb.es";
+const ip = "192.168.100.101";
+
+web = "https://victorroblescursos.es";
+// ip = "22232";
+
+console.log(web, ip);

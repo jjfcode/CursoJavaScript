7e501d3dc87afa01d6a9f87c68c1bb9b7c3684c2
John Ferlito <jjfcode@gmail.com>
2023-07-27 21:26:41 -0400

    31 Alertas y ventanas de ingreso de datos

---
+++ Hola Mundo/JS/10-ventanas.js
@@ -0,0 +1,14 @@
+'use strict'
+
+//Alerta
+
+alert("Esta es mi alerta");
+alert("Este es mi texto");
+
+// Confirmacion
+var mi_resultado = confirm("Estas seguro de querer continuar?");
+console.log(mi_resultado);
+
+// Ingreso Datos
+var mi_datos = prompt("Que edad tienes?", 18);
+console.log(typeof mi_datos);

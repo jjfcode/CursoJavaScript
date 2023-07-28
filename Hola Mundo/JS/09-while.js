d1cf581e56a409e835e00780bb54620f8ca4dec1
John Ferlito <jjfcode@gmail.com>
2023-07-27 21:16:27 -0400

    29 Bucle Do While

--- Hola Mundo/JS/09-while.js
+++ Hola Mundo/JS/09-while.js
@@ -9,4 +9,13 @@ while(year != 1991){
     console.log("Estamos en el ano: "+year);
 
     year--;
-};
+}
+
+// Do While
+
+var year = 30;
+
+do{
+    alert("Solo cuando sea diferente a 20");
+    year--;
+}while(year > 25)

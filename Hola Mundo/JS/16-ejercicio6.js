2614bfaa0cfb77be36eb18539225be0832cd0ee3
John Ferlito <jjfcode@gmail.com>
2023-07-31 21:25:57 -0400

    38 ejercicio 6

---
+++ Hola Mundo/JS/16-ejercicio6.js
@@ -0,0 +1,19 @@
+'use strict'
+
+/*
+Hacer un programa que nos diga si un numero es par o impar.
+1. Ventana prompt
+2. Si no es valido que nos pida de nuevo el numero
+*/
+
+var number = parseInt(prompt('Introduce un numero', 0));
+
+while(isNaN(number)){
+    number = parseInt(prompt('Introduce un numero', 0));
+}
+
+if(number % 2 == 0){
+    alert("Es un numero par");
+}else{
+    alert('Es impar');
+}

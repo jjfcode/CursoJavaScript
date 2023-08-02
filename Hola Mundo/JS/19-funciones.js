aa63867ba3c0f3c27b83bbf6a215c0ccc66a0ac2
John Ferlito <jjfcode@gmail.com>
2023-08-01 22:23:04 -0400

    43 parametros opcionales

--- Hola Mundo/JS/19-funciones.js
+++ Hola Mundo/JS/19-funciones.js
@@ -4,22 +4,39 @@
 // Una funcion es una agrupacion reutilizable de un conjunto de instrucciones
 
 // Defino la funcion
-function calculadora(numero1, numero2){
+function calculadora(numero1, numero2, mostrar = false){
     //conjunto de instrucciones a ejecutar
-    console.log('Suma: ' + (numero1+numero2));
-    console.log('Resta: ' + (numero1-numero2));
-    console.log('Multiplicacion: ' + (numero1*numero2));
-    console.log('Division: ' + (numero1/numero2));
-    console.log("**************************************");
+
+    if(mostrar == false){
+        console.log('Suma: ' + (numero1+numero2));
+        console.log('Resta: ' + (numero1-numero2));
+        console.log('Multiplicacion: ' + (numero1*numero2));
+        console.log('Division: ' + (numero1/numero2));
+        console.log("**************************************");
+    }else{
+        document.write('Suma: ' + (numero1+numero2)+"<br/>");
+        document.write('Resta: ' + (numero1-numero2)+"<br/>");
+        document.write('Multiplicacion: ' + (numero1*numero2)+"<br/>");
+        document.write('Division: ' + (numero1/numero2)+"<br/>");
+        document.write("**************************************"+"<br/>");
+    }
+    // console.log('Suma: ' + (numero1+numero2));
+    // console.log('Resta: ' + (numero1-numero2));
+    // console.log('Multiplicacion: ' + (numero1*numero2));
+    // console.log('Division: ' + (numero1/numero2));
+    // console.log("**************************************");
 
     // return "Hola soy la calculadora!!";
 }
 
 // Invocar o llamar a la funcion
-// calculadora(12, 8);
+calculadora(12, 8);
+calculadora(2, 5, true);
 // calculadora(98, 2);
 
+/*
 for(var i = 1; i <= 10; i++){
     console.log(i);
     calculadora(i, 2);
 }
+*/

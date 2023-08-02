c338fdeaa88fd9d9d58f9d8224c48a7de6ce14fd
John Ferlito <jjfcode@gmail.com>
2023-08-01 22:29:32 -0400

    44 funciones dentro de otras

--- Hola Mundo/JS/19-funciones.js
+++ Hola Mundo/JS/19-funciones.js
@@ -4,34 +4,36 @@
 // Una funcion es una agrupacion reutilizable de un conjunto de instrucciones
 
 // Defino la funcion
+function porConsola(numero1, numero2){
+    console.log('Suma: ' + (numero1+numero2));
+    console.log('Resta: ' + (numero1-numero2));
+    console.log('Multiplicacion: ' + (numero1*numero2));
+    console.log('Division: ' + (numero1/numero2));
+    console.log("**************************************");
+}
+
+function porPantalla(numero1, numero2){
+    document.write('Suma: ' + (numero1+numero2)+"<br/>");
+    document.write('Resta: ' + (numero1-numero2)+"<br/>");
+    document.write('Multiplicacion: ' + (numero1*numero2)+"<br/>");
+    document.write('Division: ' + (numero1/numero2)+"<br/>");
+    document.write("**************************************"+"<br/>");
+}
+
 function calculadora(numero1, numero2, mostrar = false){
-    //conjunto de instrucciones a ejecutar
-
     if(mostrar == false){
-        console.log('Suma: ' + (numero1+numero2));
-        console.log('Resta: ' + (numero1-numero2));
-        console.log('Multiplicacion: ' + (numero1*numero2));
-        console.log('Division: ' + (numero1/numero2));
-        console.log("**************************************");
+        porConsola(numero1, numero2);
     }else{
-        document.write('Suma: ' + (numero1+numero2)+"<br/>");
-        document.write('Resta: ' + (numero1-numero2)+"<br/>");
-        document.write('Multiplicacion: ' + (numero1*numero2)+"<br/>");
-        document.write('Division: ' + (numero1/numero2)+"<br/>");
-        document.write("**************************************"+"<br/>");
+        porPantalla(numero1, numero2);
     }
-    // console.log('Suma: ' + (numero1+numero2));
-    // console.log('Resta: ' + (numero1-numero2));
-    // console.log('Multiplicacion: ' + (numero1*numero2));
-    // console.log('Division: ' + (numero1/numero2));
-    // console.log("**************************************");
 
-    // return "Hola soy la calculadora!!";
+    return true
 }
 
 // Invocar o llamar a la funcion
 calculadora(12, 8);
 calculadora(2, 5, true);
+calculadora(10, 10, true)
 // calculadora(98, 2);
 
 /*

b87878892cb3a698be0ad0249f0ddd2f4b5cb92d
John Ferlito <jjfcode@gmail.com>
2023-08-01 22:04:11 -0400

    41 crear funciones

---
+++ Hola Mundo/JS/19-funciones.js
@@ -0,0 +1,18 @@
+'use strict'
+
+// Funciones
+// Una funcion es una agrupacion reutilizable de un conjunto de instrucciones
+
+// Defino la funcion
+function calculadora(){
+    //conjunto de instrucciones a ejecutar
+    console.log('Hola soy la calculadora');
+    console.log('Si soy yo');
+
+    return "Hola soy la calculadora!!";
+}
+
+// Invocar o llamar a la funcion
+var resultado = calculadora();
+
+console.log(resultado);

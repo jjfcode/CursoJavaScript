d198fa2fcba700c7da76bcfebdb304a5f79a9e44
John Ferlito <jjfcode@gmail.com>
2023-08-04 20:42:25 -0400

    46 funciones anonimas y callbacks

---
+++ Hola Mundo/JS/21-finciones-anonimas.js
@@ -0,0 +1,24 @@
+'use strict'
+
+// Funciones anonimas
+// Es una funcion que no tiene nombre
+
+// var peliculas = function(nombre){
+//     return "La pelicula es: "+nombre;
+// }
+
+function sumame(numero1, numero2, sumaYmuestra, sumaPorDos){
+    var sumar = numero1 + numero2;
+
+    sumaYmuestra(sumar);
+    sumaPorDos(sumar);
+    
+    return sumar;
+}
+
+sumame(5,7, function(dato){
+    console.log('La suma es: ', dato);
+},
+function(dato){
+    console.log('La suma por dos es: ', (dato*2));
+});
